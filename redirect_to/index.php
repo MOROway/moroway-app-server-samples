f2309ef2fcaf424ba7339c850ef995cbe9b9594c
Jonathan Herrmann-Engel <38504994+jonathanherrmannengel@users.noreply.github.com>
2024-05-26 08:51:54 +0200

    update 20240526-085154

--- redirect_to/index.php
+++ redirect_to/index.php
@@ -1,9 +1,7 @@
 <?php
 //MOROway App Server Sample "Redirect To"
 if (isset($_GET["key"])) {
-	/*
-	   Keys currently in usage by MOROway App: See file ./keys.txt
-	   Server notes can also contain redirect to keys in 'link' and 'imageLink': See file ../json_content/index.php
-   */
+	// Keys currently in usage by MOROway App: See file ./keys.txt
+	// Server notes can also contain redirect to keys in 'link' and 'imageLink': See file ../json_content/index.php
 	echo "redirecting… not";
 }
